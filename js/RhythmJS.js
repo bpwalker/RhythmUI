28b3f78b0396a72ec2233e492eadb623581abdb3
Brandon Walker <brandon.p.walker@gmail.com>
2013-10-01 23:11:31 -0400

    Script injection is finished. Still need to handle errors, refactoring constants, and unpacking chrome from jar based on OS.

---
+++ js/RhythmJS.js
@@ -0,0 +1,47 @@
+var callBacks = {}; 
+var socket = new WebSocket("ws://localhost:8686/rhythm");
+var socketInitialized = false;
+var preSockInitCalls = [];
+
+if(!socket){
+	window.close();
+}
+
+var rhythm = new RhythmUI();
+window.rhythm = rhythm;
+
+socket.onmessage = function(event) {
+	var data = JSON.parse(event.data);
+   	var cb = callBacks[data.methodId];
+   	if(cb){
+   		delete callBacks[data.methodId];
+   		cb(data.results);
+   	}
+}
+
+socket.onopen = function(event) {
+	socketInitialized = true;
+	//Send stored calls
+	preSockInitCalls.forEach(function(e){
+		socket.send(e);
+	});
+};
+	
+socket.onclose = function(event) {
+   window.close();
+};
+
+function RhythmUI(){
+	this.call = function(method, parameters, cb){
+		var methodId =  method + "_" + (new Date()).getTime();
+		callBacks[methodId] = cb;
+		var params = JSON.stringify({"method" : method, "parameters" : parameters, "methodId" : methodId });
+		
+		//Check if socket has been initialized. If not, save it to array to be sent after initialization.
+		if(socketInitialized){
+			socket.send(params);
+		} else{
+			preSockInitCalls.push(params);
+		}
+	}	
+};
--- src/org/rhythm/RhythmUI.groovy
+++ src/org/rhythm/RhythmUI.groovy
@@ -5,7 +5,11 @@ import groovy.json.JsonSlurper
 
 import javax.swing.text.html.HTMLDocument
 import javax.swing.text.html.HTMLEditorKit
+import javax.swing.text.Element
+import javax.swing.text.StyleConstants
+import javax.swing.text.html.HTML
 
+import org.cyberneko.html.parsers.SAXParser
 import org.vertx.groovy.core.Vertx
 import org.vertx.groovy.core.buffer.Buffer
 import org.vertx.groovy.core.http.HttpServer
@@ -26,21 +30,31 @@ class RhythmUI {
 	
 	private methods = [:];
 	
-	
+	/**
+	 * Constructor with html filepath setter
+	 * @param filePath
+	 */
 	public RhythmUI(String filePath) {
-		super();
 		this.filePath = filePath;
 	}
 	
 	public RhythmUI() {
-		super();
 	}
 	
+	/**
+	 * Gets server instance and sets the html filepath
+	 * @param filePath Directory to set html path
+	 * @return The Rhythm vert.x server
+	 */
 	public HttpServer getServerInstance(String filePath){
 		this.filePath = filePath;
 		return getServerInstance();
 	}
 	
+	/**
+	 * Singleton for Rhythms vert.x server
+	 * @return
+	 */
 	public HttpServer getServerInstance(){
 		if(server == null){
 			server = vertx.createHttpServer();
@@ -54,26 +68,29 @@ class RhythmUI {
 				vertx.fileSystem.readFile(constructFilePath(file)) { ar ->
 					if (ar.succeeded) {
 						try {
-							//stringReader = new StringReader(ar.result.toString());
-				        	//page = (HTMLDocument) htmlKit.createDefaultDocument();
-							//htmlKit.read(stringReader, page, 0);
-							//Element endOfPage = page.getElement(page.getDefaultRootElement(), StyleConstants.NameAttribute, HTML.Tag.HTML);
-					        //page.insertBeforeEnd(endOfPage, "<div>inject</div>");
-					        //StringWriter stringWriter = new StringWriter();
-					        //htmlKit.write(stringWriter, page, 0, page.getLength());
+							def root = new XmlParser(new org.cyberneko.html.parsers.SAXParser()).parseText(ar.result.toString());
+							
+							def body = root."**".findAll { 
+								try{ 
+									return it.name().equalsIgnoreCase("body");
+								} catch(Exception e) { 
+									return false;
+								} 
+							}[0];
+						
+							body.children().add(0, new Node(null, 'script', [type : "text/javascript"], new File("js/RhythmJS.js").text));
+						
+							def writer = new StringWriter();
+							new XmlNodePrinter(new PrintWriter(writer)).print(root);
+							request.response.end(writer.toString());
 
-							//request.response.end(stringWriter.toString());
-							
-							request.response.end(ar.result.toString());
-							
-							
-							//TODO: Inject javascript library
 						} catch (Exception e) {
 							//TODO: Fail
 							e.printStackTrace();
 							return;
 						}
 					} else {
+						System.err.println("Failed to find specified file: " + constructFilePath(file));
 						//TODO: Error message
 					}
 				};
@@ -102,6 +119,11 @@ class RhythmUI {
 		return server;
 	}
 
+	/**
+	 * Starts Rhythm's UI with Chrome
+	 * @param file The initial page to navigate to within the browser
+	 * @return The process started by executing Chrome
+	 */
 	public Process startUI(String file) {
 		// start chrome with flag and page
 		server = getServerInstance();
@@ -116,26 +138,65 @@ class RhythmUI {
 		
 		return process;
 	}
+	
+	/**
+	 * Maps a closure to be triggered by a specific name
+	 * @param name Name to trigger closure
+	 * @param function Closure to trigger
+	 */
 	public void on(String name, Closure function){
-		methods.put(name, function);		
+		methods.putAt(name, function);		
 	}
 	
-	public def on(Object object, String method){
+	/**
+	 * Maps a closure to be triggered on a specific object by name
+	 * @param object The object of the method
+	 * @param method The name of the method ont he object and name to trigger method
+	 */
+	public void on(Object object, String method){
 		methods.putAt(method, object.&"$method");
 	}
+	
+	/**
+	 * Maps a named method to be triggered on a specific object by a reference name
+	 * @param object The object of the method
+	 * @param method The name of the method to trigger on the object
+	 * @param name The name to trigger the method
+	 */
+	public void on(Object object, String method, String name){
+		on(name, object.&"$method");
+	}
 
+	/**
+	 * Changes the html filepath
+ 	 * @param filePath The filepath for the html files
+	 */
 	public void changeFilePath(String filePath) {
 		this.filePath = filePath;
 	}
 	
+	/**
+	 * Creates the absolute file path based on the file for the OS
+	 * @param file The file to get the absolute file path for
+	 * @return The absolute file path string to the file
+	 */
 	public String constructFilePath(String file){
 		return getLocalPath() + file.replace("/",  "\\") ;
 	}
 	
+	/**
+	 * Creates the URL path to navigate to a specific file
+	 * @param file The file to get the URL path to
+	 * @return The URL string to the file
+	 */
 	public String constructWebPath(String file){
 		return "http://localhost:" + SERVER_PORT + "/" + filePath + "/" + file + "\"";
 	}
 	
+	/**
+	 * Gets the local path to the current directory
+	 * @return The user's current directory
+	 */
 	public static String getLocalPath(){
 		return System.getProperty("user.dir");
 	}
